48d4c12956cd2a9360b05034d95ef65210174d81
Saro0h <mkhalil.sarah@gmail.com>
2014-10-12 00:13:37 +0200

    My first time sending email with RabbitMQ

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Poledev\AsynchronousBundle\PoledevAsynchronousBundle(),
+            new OldSound\RabbitMqBundle\OldSoundRabbitMqBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
